1ed2b39aff4102c9f17f7cafe1d65e690026c60e
AlexZGreat <alexzgreat1093@gmail.com>
2026-08-10 18:46:05 -0400

    Add files via upload

---
+++ Localization/en-us.lua
@@ -0,0 +1,309 @@
+return {
+    descriptions = {
+        Back={},
+        Blind={
+            bl_ven_wheat = {
+                name = 'The Wheat',
+                text = {
+                    '+#1# hand size,',
+                    '-#2# hand size after',
+                    'each hand played or',
+                    'discarded',
+                },
+            },
+            bl_ven_slope = {
+                name = 'The Slope',
+                text = {
+                    'Curse cards played with',
+                    'cards of consecutive',
+                    'rank with Slippery',
+                },
+            },
+            bl_ven_thorn = {
+                name = 'The Thorn',
+                text = {
+                    'Curse all played face',
+                    'cards with Bleeding',
+                },
+            },
+            bl_ven_comb = {
+                name = 'The Comb',
+                text = {
+                    'Discard 2/3ds of your',
+                    'deck on blind select',
+                },
+            },
+            bl_ven_pin = {
+                name = 'The Pin',
+                text = {
+                    'Curse all played cards',
+                    'not on the edge of',
+                    'a hand with Bleeding',
+                },
+            },
+            bl_ven_crescent = {
+                name = 'The Crescent',
+                text = {
+                    'Curse all played',
+                    'black suited cards',
+                    'with Voided',
+                },
+            },
+            bl_ven_cross = {
+                name = 'The Cross',
+                text = {
+                    'Debuff cards previously',
+                    'debuffed this run',
+                },
+            },
+            bl_ven_axe = {
+                name = 'The Axe',
+                text = {
+                    'Destroy all played',
+                    'enhanced cards',
+                },
+            },
+            bl_ven_fence = {
+                name = 'The Fence',
+                text = {
+                    'Curse all played cards',
+                    'on the edge of a hand',
+                    'with Disconnected',
+                },
+            },
+            bl_ven_scratch = {
+                name = 'The Scratch',
+                text = {
+                    'Curse all played',
+                    'red suited cards',
+                    'with Disconnected',
+                },
+            },
+            bl_ven_pole = {
+                name = 'The Pole',
+                text = {
+                    'Hand must contain',
+                    'an Ace or 2',
+                },
+            },
+            bl_ven_whirlpool = {
+                name = 'The Whirlpool',
+                text = {
+                    'Curse #1# in #2# discarded',
+                    'cards with Slippery',
+                },
+            },
+            bl_ven_crowbar = {
+                name = 'The Crowbar',
+                text = {
+                    'Score resets',
+                    'on discard',
+                },
+            },
+            bl_ven_door = {
+                name = 'The Door',
+                text = {
+                    'Only highest',
+                    'card scores',
+                },
+            },
+            bl_ven_trident = {
+                name = 'The Trident',
+                text = {
+                    'All hands are',
+                    'Three of a Kind',
+                },
+            },
+            bl_ven_bolt = {
+                name = 'The Bolt',
+                text = {
+                    'When playing a hand,',
+                    'curse all cards held',
+                    'in hand with Blinded',
+                },
+            },
+            bl_ven_arrow = {
+                name = 'The Arrow',
+                text = {
+                    'Curse #1# in #2# played',
+                    'cards with Blinded',
+                },
+            },
+            bl_ven_knuckle = {
+                name = 'The Knuckle',
+                text = {
+                    'No retriggers',
+                },
+            },
+            bl_ven_cat = {
+                name = 'The Cat',
+                text = {
+                    'Cards score as',
+                    'if held in hand',
+                },
+            },
+            bl_ven_gate = {
+                name = 'The Gate',
+                text = {
+                    'Score resets after',
+                    'every hand',
+                },
+            },
+            bl_ven_cane = {
+                name = 'The Cane',
+                text = {
+                    'Debuff all',
+                    'cursed cards',
+                },
+            },
+            bl_ven_helix = {
+                name = 'The Helix',
+                text = {
+                    'Hand must contain at',
+                    'least #1# different suits',
+                },
+            },
+            bl_ven_hourglass = {
+                name = 'The Hourglass',
+                text = {
+                    'Cards played/discarded',
+                    'after first play/discard',
+                    'are cursed with Voided',
+                },
+            },
+            bl_ven_twist = {
+                name = 'The Twist',
+                text = {
+                    'Jokers scale #1#x as',
+                    'fast, but negatively',
+                },
+            },
+            bl_ven_the_blind = {
+                name = 'The... Blind?',
+                text = {
+                    'This is the only',
+                    'blind this ante',
+                },
+            },
+            bl_ven_copper_coin = {
+                name = 'Copper Coin',
+                text = {
+                    '+#1# hand, but',
+                    'extremely large blind',
+                },
+            },
+            bl_ven_silver_sword = {
+                name = 'Silver Sword',
+                text = {
+                    'When first beaten,',
+                    'score resets instead',
+                },
+            },
+            bl_ven_ebony_wand = {
+                name = 'Ebony Wand',
+                text = {
+                    'Curse first #1#',
+                    'cards drawn with',
+                    'a random curse',
+                },
+            },
+            bl_ven_azure_rose = {
+                name = 'Azure Rose',
+                text = {
+                    'X#1# blind size after',
+                    'hand or discard',
+                },
+            },
+            bl_ven_ivory_shield = {
+                name = 'Ivory Shield',
+                text = {
+                    'Shuffle a standard',
+                    '52-card deck',
+                    'into your deck',
+                },
+            },
+        },
+        Edition={},
+        Enhanced={},
+        Joker={},
+        Other={
+            ven_blinded = {
+                name = 'Blinded',
+                text = {
+                    "{C:inactive}You can't tell",
+                    '{C:inactive}what card this is',
+                },
+            },
+            ven_bleeding = {
+                name = 'Bleeding',
+                text = {
+                    "When gaining {C:chips}chips,",
+                    'removes {C:attention}twice{} that instead',
+                },
+            },
+            ven_slippery = {
+                name = 'Slippery',
+                text = {
+                    "When being {C:attention}played{} or",
+                    '{C:attention}discarded{}, has a {C:green}#1#{} in',
+                    '{C:green}#2#{} chance to... {C:red}not'
+                },
+            },
+            ven_voided = {
+                name = 'Voided',
+                text = {
+                    "Does not trigger",
+                    '{C:attention}joker{} effects',
+                },
+            },
+            ven_disconnected = {
+                name = 'Disconnected',
+                text = {
+                    "Cannot be selected",
+                    'alongside #1# other',
+                    "{C:attention}Disconnected{} cards",
+                },
+            },
+        },
+        Planet={},
+        Spectral={},
+        Stake={},
+        Tag={},
+        Tarot={
+            c_ven_the_eye = {
+                name = 'The Eye',
+                text = {
+                    'Remove {C:attention}curses from {C:attention}all',
+                    'cards in hand and',
+                    'create another {C:tarot}Tarot{} card'
+                },
+            },
+        },
+        Voucher={},
+    },
+    misc = {
+        achievement_descriptions={},
+        achievement_names={},
+        blind_states={},
+        challenge_names={},
+        collabs={},
+        dictionary={},
+        high_scores={},
+        labels={
+            ven_blinded = "Blinded",
+            ven_bleeding = "Bleeding",
+            ven_slippery = "Slippery",
+            ven_voided = "Voided",
+            ven_disconnected = "Disconnected",
+        },
+        poker_hand_descriptions={},
+        poker_hands={},
+        quips={},
+        ranks={},
+        suits_plural={},
+        suits_singular={},
+        tutorial={},
+        v_dictionary={},
+        v_text={},
+    },
+}
