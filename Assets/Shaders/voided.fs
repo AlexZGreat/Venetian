1259e85afa5d0dd0e902026c785e19c20d794739
AlexZGreat <alexzgreat1093@gmail.com>
2026-08-10 18:42:53 -0400

    Add files via upload

---
+++ Assets/Shaders/voided.fs
@@ -0,0 +1,227 @@
+#if defined(VERTEX) || __VERSION__ > 100 || defined(GL_FRAGMENT_PRECISION_HIGH)
+    #define PRECISION highp
+#else
+    #define PRECISION mediump
+#endif
+
+// !! change this variable name to your Shader's name
+// YOU MUST USE THIS VARIABLE IN THE vec4 effect AT LEAST ONCE
+
+// Values of this variable:
+// self.ARGS.send_to_shader[1] = math.min(self.VT.r*3, 1) + (math.sin(G.TIMERS.REAL/28) + 1) + (self.juice and self.juice.r*20 or 0) + self.tilt_var.amt
+// self.ARGS.send_to_shader[2] = G.TIMERS.REAL
+extern PRECISION vec2 voided;
+
+extern PRECISION number dissolve;
+extern PRECISION number time;
+extern PRECISION number time_but_works;
+// [Note] sprite_pos_x _y is not a pixel position!
+//        To get pixel position, you need to multiply  
+//        it by sprite_width _height (look flipped.fs)
+// (sprite_pos_x, sprite_pos_y, sprite_width, sprite_height) [not normalized]
+extern PRECISION vec4 texture_details;
+// (width, height) for atlas texture [not normalized]
+extern PRECISION vec2 image_details;
+extern bool shadow;
+extern PRECISION vec4 burn_colour_1;
+extern PRECISION vec4 burn_colour_2;
+
+// [Required] 
+// Apply dissolve effect (when card is being "burnt", e.g. when consumable is used)
+vec4 dissolve_mask(vec4 tex, vec2 texture_coords, vec2 uv);
+
+vec2 random2(vec2 st){
+    st = vec2( dot(st,vec2(127.1,311.7)),
+              dot(st,vec2(269.5,183.3)) );
+    return -1.0 + 2.0*fract(sin(st)*43758.5453123);
+}
+
+// Gradient Noise by Inigo Quilez - iq/2013
+// https://www.shadertoy.com/view/XdXGW8
+float noise(vec2 st) {
+    vec2 i = floor(st);
+    vec2 f = fract(st);
+
+    vec2 u = f*f*(3.0-2.0*f);
+
+    return mix( mix( dot( random2(i + vec2(0.0,0.0) ), f - vec2(0.0,0.0) ),
+                     dot( random2(i + vec2(1.0,0.0) ), f - vec2(1.0,0.0) ), u.x),
+                mix( dot( random2(i + vec2(0.0,1.0) ), f - vec2(0.0,1.0) ),
+                     dot( random2(i + vec2(1.0,1.0) ), f - vec2(1.0,1.0) ), u.x), u.y);
+}
+
+// Gradient Noise by Inigo Quilez - iq/2013
+// https://www.shadertoy.com/view/XdXGW8
+vec2 vnoise(vec2 st){
+    vec2 base_cell = floor(st);
+    float min_distance_to_cell = 10.0;
+    vec2 closest_cell;
+    for (int x=-1; x<=1; x++){
+        for (int y=-1; y<=1; y++){
+        	vec2 cell = base_cell+vec2(x,y);
+            vec2 position_in_cell = random2(cell)*0.5+0.5;
+            float dist = distance(st,cell+position_in_cell);
+            if(dist < min_distance_to_cell){
+                min_distance_to_cell = dist;
+                closest_cell = cell;
+            }
+    	}
+    }
+    
+	float min_distance_to_edge = 10.0;
+    for (int x=-1; x<=1; x++){
+        for (int y=-1; y<=1; y++){
+        	vec2 cell = base_cell+vec2(x,y);
+            vec2 position_in_cell = random2(cell);
+            position_in_cell = 0.5 + 0.5*sin(time_but_works + 6.2831*position_in_cell);
+            position_in_cell.x = min(abs(position_in_cell.x),0.5);
+            position_in_cell.x = min(abs(position_in_cell.y),0.5);
+            vec2 to_cell = cell+position_in_cell-st;
+            float dist = distance(st,cell+position_in_cell);
+            vec2 diff_from_closest_cell = abs(closest_cell - cell);
+            bool is_closest_cell = diff_from_closest_cell.x + diff_from_closest_cell.x <0.1;
+            if(!is_closest_cell){
+                vec2 to_center = (min_distance_to_cell + to_cell) * 0.5;
+                vec2 cell_difference = normalize(to_cell - min_distance_to_cell);
+                float edge_distance = dot(to_center, cell_difference);
+                min_distance_to_edge = min(min_distance_to_edge,edge_distance);
+            }
+    	}
+    }
+    return vec2(min_distance_to_cell,min_distance_to_edge);
+}
+
+vec2 vnoise_taxicab(vec2 st){
+    vec2 base_cell = floor(st);
+    float min_distance_to_cell = 10.0;
+    vec2 closest_cell;
+    for (int x=-1; x<=1; x++){
+        for (int y=-1; y<=1; y++){
+        	vec2 cell = base_cell+vec2(x,y);
+            vec2 position_in_cell = random2(cell)*0.5+0.5;
+            float dist = abs(st-cell+position_in_cell).x + abs(st-cell+position_in_cell).y;
+            if(dist < min_distance_to_cell){
+                min_distance_to_cell = dist;
+                closest_cell = cell;
+            }
+    	}
+    }
+    
+	float min_distance_to_edge = 10.0;
+    for (int x=-1; x<=1; x++){
+        for (int y=-1; y<=1; y++){
+        	vec2 cell = base_cell+vec2(x,y);
+            vec2 position_in_cell = random2(cell);
+            position_in_cell = 0.5 + 0.5*sin(time_but_works + 6.2831*position_in_cell);
+            position_in_cell.x = min(abs(position_in_cell.x),0.5);
+            position_in_cell.x = min(abs(position_in_cell.y),0.5);
+            vec2 to_cell = cell+position_in_cell-st;
+            float dist = distance(st,cell+position_in_cell);
+            vec2 diff_from_closest_cell = abs(closest_cell - cell);
+            bool is_closest_cell = diff_from_closest_cell.x + diff_from_closest_cell.x <0.1;
+            if(!is_closest_cell){
+                vec2 to_center = (min_distance_to_cell + to_cell) * 0.5;
+                vec2 cell_difference = normalize(to_cell - min_distance_to_cell);
+                float edge_distance = dot(to_center, cell_difference);
+                min_distance_to_edge = min(min_distance_to_edge,edge_distance);
+            }
+    	}
+    }
+    return vec2(min_distance_to_cell,min_distance_to_edge);
+}
+
+// This is what actually changes the look of card
+vec4 effect( vec4 colour, Image texture, vec2 texture_coords, vec2 screen_coords )
+{
+    // Take pixel color (rgba) from `texture` at `texture_coords`, equivalent of texture2D in GLSL
+    vec4 tex = Texel(texture, texture_coords);
+    // Position of a pixel within the sprite
+	vec2 uv = (((texture_coords)*(image_details)) - texture_details.xy*texture_details.ba)/texture_details.ba;
+
+    vec3 color1 = vec3(0.087,0.012,0.105);
+    vec3 color2 = vec3(0.192,0.016,0.255);
+    vec3 color3 = vec3(0.275,0.264,0.490);
+    vec3 color4 = vec3(0.720,0.717,0.820);
+    vec3 color5 = vec3(0.990,0.989,0.852);
+
+    screen_coords = vec2(screen_coords.x,-screen_coords.y);
+    
+    float blend = (noise(screen_coords/300.0*1.0+500.0*sin(time_but_works/500.0))*0.5+0.5);
+    
+    vec3 color = mix(color1,color2,vec3(smoothstep(0.3,0.8,blend)));
+    
+    float blend2 = min(vnoise(screen_coords/300.0*10.0+time_but_works/20.0).x+0.5,vnoise(500.0+screen_coords/300.0*10.0+time_but_works/20.0).x+0.5);
+    
+    color = mix(color,color3,vec3(0.5*smoothstep(0.7,1.1,blend2)));
+    
+    float blend3 = (vnoise_taxicab(screen_coords/300.0*7.0+time_but_works/10.0).x+0.5);
+    
+    color = mix(color,color4,vec3(smoothstep(0.63,0.52,blend3)));
+    
+    float blend4 = (vnoise_taxicab(screen_coords/300.0*3.0+time_but_works/8.0).x+0.5);
+    
+    color = mix(color,color5,vec3(smoothstep(0.58,0.54,blend4)));
+
+    colour = vec4(color.r,color.g,color.b,tex.a);
+
+    // required
+    return dissolve_mask(colour, texture_coords, uv);
+}
+
+vec4 dissolve_mask(vec4 tex, vec2 texture_coords, vec2 uv)
+{
+    if (dissolve < 0.001) {
+        return vec4(shadow ? vec3(0.,0.,0.) : tex.xyz, shadow ? tex.a*0.3: tex.a);
+    }
+
+    float adjusted_dissolve = (dissolve*dissolve*(3.-2.*dissolve))*1.02 - 0.01; //Adjusting 0.0-1.0 to fall to -0.1 - 1.1 scale so the mask does not pause at extreme values
+
+	float t = time * 10.0 + 2003.;
+	vec2 floored_uv = (floor((uv*texture_details.ba)))/max(texture_details.b, texture_details.a);
+    vec2 uv_scaled_centered = (floored_uv - 0.5) * 2.3 * max(texture_details.b, texture_details.a);
+	
+	vec2 field_part1 = uv_scaled_centered + 50.*vec2(sin(-t / 143.6340), cos(-t / 99.4324));
+	vec2 field_part2 = uv_scaled_centered + 50.*vec2(cos( t / 53.1532),  cos( t / 61.4532));
+	vec2 field_part3 = uv_scaled_centered + 50.*vec2(sin(-t / 87.53218), sin(-t / 49.0000));
+
+    float field = (1.+ (
+        cos(length(field_part1) / 19.483) + sin(length(field_part2) / 33.155) * cos(field_part2.y / 15.73) +
+        cos(length(field_part3) / 27.193) * sin(field_part3.x / 21.92) ))/2.;
+    vec2 borders = vec2(0.2, 0.8);
+
+    float res = (.5 + .5* cos( (adjusted_dissolve) / 82.612 + ( field + -.5 ) *3.14))
+    - (floored_uv.x > borders.y ? (floored_uv.x - borders.y)*(5. + 5.*dissolve) : 0.)*(dissolve)
+    - (floored_uv.y > borders.y ? (floored_uv.y - borders.y)*(5. + 5.*dissolve) : 0.)*(dissolve)
+    - (floored_uv.x < borders.x ? (borders.x - floored_uv.x)*(5. + 5.*dissolve) : 0.)*(dissolve)
+    - (floored_uv.y < borders.x ? (borders.x - floored_uv.y)*(5. + 5.*dissolve) : 0.)*(dissolve);
+
+    if (tex.a > 0.01 && burn_colour_1.a > 0.01 && !shadow && res < adjusted_dissolve + 0.8*(0.5-abs(adjusted_dissolve-0.5)) && res > adjusted_dissolve) {
+        if (!shadow && res < adjusted_dissolve + 0.5*(0.5-abs(adjusted_dissolve-0.5)) && res > adjusted_dissolve) {
+            tex.rgba = burn_colour_1.rgba;
+        } else if (burn_colour_2.a > 0.01) {
+            tex.rgba = burn_colour_2.rgba;
+        }
+    }
+
+    return vec4(shadow ? vec3(0.,0.,0.) : tex.xyz, res > adjusted_dissolve ? (shadow ? tex.a*0.3: tex.a) : .0);
+}
+
+// for transforming the card while your mouse is on it
+extern PRECISION vec2 mouse_screen_pos;
+extern PRECISION float hovering;
+extern PRECISION float screen_scale;
+
+#ifdef VERTEX
+vec4 position( mat4 transform_projection, vec4 vertex_position )
+{
+    if (hovering <= 0.){
+        return transform_projection * vertex_position;
+    }
+    float mid_dist = length(vertex_position.xy - 0.5*love_ScreenSize.xy)/length(love_ScreenSize.xy);
+    vec2 mouse_offset = (vertex_position.xy - mouse_screen_pos.xy)/screen_scale;
+    float scale = 0.2*(-0.03 - 0.3*max(0., 0.3-mid_dist))
+                *hovering*(length(mouse_offset)*length(mouse_offset))/(2. -mid_dist);
+
+    return transform_projection * vertex_position + vec4(0,0,0,scale);
+}
+#endif
